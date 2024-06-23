f9cb53e05dbe1c860f357858630992f1958872b6
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-06-23 04:16:25 -0400

    Create setup_settings.sh

---
+++ setup_settings.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create or edit settings.json
+echo '{
+  "global": {
+    "general": {
+      "download_path": "./downloads/",
+      "download_quality": "hifi",
+      "search_limit": 10
+    },
+    "module_defaults": {
+      "lyrics": "default",
+      "covers": "default",
+      "credits": "default"
+    }
+  },
+  "extensions": {},
+  "modules": {
+    "qobuz": {
+      "app_id": "950096963",
+      "app_secret": "979549437fcc4a3faad4867b5cd25dcb",
+      "quality_format": "{sample_rate}kHz {bit_depth}bit",
+      "username": "'"$QOBUZ_USERNAME"'",
+      "password": "'"$QOBUZ_PASSWORD"'"
+    }
+  }
+}' > config/settings.json
